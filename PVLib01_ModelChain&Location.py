66db1955bafd48668cad87b7c603ad7dcb88b44a
Pier Luca Anania <p.l.anania@gmail.com>
2024-03-10 15:25:52 +0100

    PVLib01 Upload

--- PVLib01_ModelChain&Location.py
+++ PVLib01_ModelChain&Location.py
@@ -1,4 +1,6 @@
 from pvlib.location import Location
+from pvlib.pvsystem import PVSystem
+
 
 # Rome, Italy data
 latitude = 41.9
@@ -9,11 +11,9 @@ name = 'Rome'
 
 location = Location(latitude, longitude, tz, altitude, name)
 
-tus = Location(32.2, -111, 'US/Arizona', 700, 'Tucson')
-print(tus)
-golden = Location(39.742476, -105.1786, 'America/Denver', 1830, 'Golden')
-print(golden)
-golden_mst = Location(39.742476, -105.1786, 'MST', 1830, 'Golden MST')
-print(golden_mst)
-berlin = Location(52.5167, 13.3833, 'Europe/Berlin', 34, 'Berlin')
-print(berlin)
+'''Create Instance'''
+system = PVSystem(surface_tilt=45,
+                 surface_azimuth=180,
+                 )
+system
+
